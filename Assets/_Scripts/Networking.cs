4358f9f7411926402c065432f468e09e2e96a27b
Bosko Ivkovic <bosko2003414@gmail.com>
2018-09-15 15:55:16 +0200

    Class

--- Assets/_Scripts/Networking.cs
+++ Assets/_Scripts/Networking.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 public class Networking : NetworkManager {
 
 	public GameObject defaultPlayer;
+    public string whatToLoad;
 
 	public override void OnServerConnect(NetworkConnection conn)
 	{
@@ -15,7 +16,7 @@ public class Networking : NetworkManager {
 	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
 	{
         Debug.Log("spawn");
-            defaultPlayer = Instantiate(Resources.Load("class"), new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            defaultPlayer = Instantiate(Resources.Load(whatToLoad), new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
 
 			NetworkServer.AddPlayerForConnection(conn, defaultPlayer, playerControllerId);
 
