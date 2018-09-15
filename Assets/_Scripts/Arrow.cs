602d1cd601aa6005a86b56cb8c2de1d7033d4d97
Tijn van den Broek <36229295+TijnWorks@users.noreply.github.com>
2018-09-15 15:33:21 +0200

    pijl
    hier bosko hvj

---
+++ Assets/_Scripts/Arrow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float force;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * force, ForceMode.Impulse);
+    }
+
+}
